cab5551d0aa4edd8503cb8f842716848f890ba9d
Henrique-Linhares <hlinhares453@gmail.com>
2025-09-15 22:52:26 -0300

    Criação do Projeto

---
+++ src/components/AvisoTemporizado.js
@@ -0,0 +1,11 @@
+import React, {useState, UseEffect, useEffect} from 'react'
+
+function AvisoTemporizado() {
+    const [temporizador, setTemporizador] = useState(0);
+
+    useEffect(() => {
+        
+    }, []);
+};
+
+export default AvisoTemporizado;
