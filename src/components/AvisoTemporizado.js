d3ead06d16a85e07c148f8628340ae6411b1f5ce
Henrique-Linhares <hlinhares453@gmail.com>
2025-09-22 19:29:05 -0300

    Implementação da function ExibeMensagem

--- src/components/AvisoTemporizado.js
+++ src/components/AvisoTemporizado.js
@@ -1,11 +1,31 @@
-import React, {useState, UseEffect, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function AvisoTemporizado() {
-    const [temporizador, setTemporizador] = useState(0);
+    const [visibilidade, setVisibilidade] = useState(true);
 
     useEffect(() => {
-        
+        const temporizador = setTimeout(() => {
+            setVisibilidade(false);
+        }, 3000)
+
+        return(() => {
+            clearTimeout(temporizador)
+        });
     }, []);
-};
+
+    const exibeMensagem = () => {
+        if(visibilidade === true) {
+            return <h1>Bem-Vindo</h1>
+        } else {
+            return null
+        }
+    }
+
+    return(
+        <div>
+            {exibeMensagem()}
+        </div>
+    )
+}
 
 export default AvisoTemporizado;
